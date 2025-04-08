2d925f8a963236042e65f602b759bce9b2551ad0
SriDharsini21 <sridharshiniv21@gmail.com>
2025-04-08 13:35:03 +0530

    Updated Till Authentication

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ const App = () => {
           backgroundColor={Colors.WHITE}
           barStyle="dark-content"
         />
+        {/* tab navigation */}
         <TabNavigation />
       </GestureHandlerRootView>
     </Provider>
